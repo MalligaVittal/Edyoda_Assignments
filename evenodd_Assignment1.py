47dc142ffc987e309187f7afad399f279163b26a
MalligaVittal <141703757+MalligaVittal@users.noreply.github.com>
2023-08-17 10:43:37 +0530

    evenodd_Assignment1.py

--- evenodd_Assignment1.py
+++ evenodd_Assignment1.py
@@ -1,7 +1,5 @@
 # Write a Python program to count the number of even and odd numbers from a series of numbers.
-'''
-Note:
-Sample numbers : numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
+'''Note:Sample numbers : numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
 Expected Output :
 Number of even numbers : 4
 Number of odd numbers : 5
